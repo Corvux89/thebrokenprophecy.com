b3649152e03858efa7b9aed88020f8e786a1561f
corvux <bryce.hadsell@gmail.com>
2022-12-10 08:00:23 -0600

    updates per council

--- models/categories.py
+++ models/categories.py
@@ -32,8 +32,3 @@ class CharacterSubclass(db.Model):
     parent = db.Column(db.Integer)
     pass
 
-class Factions(db.Model):
-        __tablename__ = 'c_faction'
-
-        id = db.Column(db.Integer, primary_key=True)
-        value = db.Column(db.String)
