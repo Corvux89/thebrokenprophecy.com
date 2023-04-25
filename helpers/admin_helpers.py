7a724a2f18bcaed46cc83901a0dd36974ab5f926
Bryce Hadsell <bryce.hadsell@gmail.com>
2023-04-25 17:12:45 -0500

    Query updates and improvements

--- helpers/admin_helpers.py
+++ helpers/admin_helpers.py
@@ -1,67 +1,51 @@
 from flask import current_app
+from sqlalchemy import literal, union, select
 
 from models import BlackSmithItem, BlackSmithType, Rarity, ConsumableItem, ConsumableType, ScrollItem, WondrousItem, \
     CharacterRace, CharacterSubrace, CharacterClass, CharacterSubclass
 
 
 def get_item_list():
-    blacksmith_items = current_app.db.session.query(BlackSmithItem.id, BlackSmithItem.name, BlackSmithItem.cost, BlackSmithType.value.label("type"),
-                                     Rarity.value.label("rarity")) \
+    s1 = select(BlackSmithItem.id,
+                BlackSmithItem.name,
+                BlackSmithItem.cost,
+                BlackSmithType.value.label('type'),
+                Rarity.value.label('rarity'),
+                literal('Blacksmith').label('table')) \
         .join(BlackSmithType, BlackSmithItem.sub_type == BlackSmithType.id) \
         .join(Rarity, BlackSmithItem.rarity == Rarity.id)
 
-    items = []
-
-    for i in blacksmith_items:
-        i_dict = {}
-        i_dict["name"] = i.name
-        i_dict["id"] = i.id
-        i_dict["cost"] = i.cost
-        i_dict["type"] = i.type
-        i_dict["rarity"] = i.rarity
-        i_dict["table"] = "Blacksmith"
-        items.append(i_dict)
-
-    consumable_items = current_app.db.session.query(ConsumableItem.id, ConsumableItem.name, ConsumableItem.cost, ConsumableType.value.label("type"),
-                                     Rarity.value.label("rarity")) \
+    s2 = select(ConsumableItem.id,
+                ConsumableItem.name,
+                ConsumableItem.cost,
+                ConsumableType.value.label('type'),
+                Rarity.value.label('rarity'),
+                literal('Consumable').label('table')) \
         .join(ConsumableType, ConsumableItem.sub_type == ConsumableType.id) \
         .join(Rarity, ConsumableItem.rarity == Rarity.id)
 
-    for i in consumable_items:
-        i_dict = {}
-        i_dict["name"] = i.name
-        i_dict["id"] = i.id
-        i_dict["cost"] = i.cost
-        i_dict["type"] = i.type
-        i_dict["rarity"] = i.rarity
-        i_dict["table"] = "Consumable"
-        items.append(i_dict)
-
-    scroll_items = current_app.db.session.query(ScrollItem.id, ScrollItem.name, ScrollItem.cost, Rarity.value.label("rarity")) \
+    s3 = select(ScrollItem.id,
+                ScrollItem.name,
+                ScrollItem.cost,
+                literal('').label('type'),
+                Rarity.value.label('rarity'),
+                literal('Scroll').label('table')) \
         .join(Rarity, ScrollItem.rarity == Rarity.id)
 
-    for i in scroll_items:
-        i_dict = {}
-        i_dict["name"] = i.name
-        i_dict["id"] = i.id
-        i_dict["cost"] = i.cost
-        i_dict["type"] = ""
-        i_dict["rarity"] = i.rarity
-        i_dict["table"] = "Scroll"
-        items.append(i_dict)
-
-    wondrous_items = current_app.db.session.query(WondrousItem.id, WondrousItem.name, WondrousItem.cost, Rarity.value.label("rarity")) \
+    s4 = select(WondrousItem.id,
+                WondrousItem.name,
+                WondrousItem.cost,
+                literal('').label('type'),
+                Rarity.value.label('rarity'),
+                literal('Wondrous').label('table')) \
         .join(Rarity, WondrousItem.rarity == Rarity.id)
 
-    for i in wondrous_items:
-        i_dict = {}
-        i_dict["name"] = i.name
-        i_dict["id"] = i.id
-        i_dict["cost"] = i.cost
-        i_dict["type"] = ""
-        i_dict["rarity"] = i.rarity
-        i_dict["table"] = "Wondrous"
-        items.append(i_dict)
+    u = union(s1, s2, s3, s4).alias('items')
+
+    vals = current_app.db.session.query(u)
+
+    items = [{"name": v.name, "id": v.id, "cost": v.cost, "type": v.type, "rarity": v.rarity, "table": v.table} for v in
+             vals]
 
     return items
 
@@ -70,16 +54,7 @@ def get_races():
     races = current_app.db.session.query(CharacterRace)
 
     d_out = dict()
-    d_out["parents"] = []
-
-    for r in races:
-        subraces = get_subraces(r.id)
-        r_dict = {}
-        r_dict['id'] = r.id
-        r_dict['name'] = r.value
-        r_dict['children'] = len(subraces['children'])
-
-        d_out['parents'].append(r_dict)
+    d_out["parents"] = [{"id": r.id, "name": r.value, "children": len(get_subraces(r.id)['children'])} for r in races]
 
     d_out["parents"] = sorted(d_out["parents"], key=lambda r: r["name"])
 
@@ -90,14 +65,7 @@ def get_subraces(race):
     subraces = current_app.db.session.query(CharacterSubrace).filter(CharacterSubrace.parent == race)
 
     d_out = dict()
-    d_out['children'] = []
-
-    for s in subraces:
-        s_dict = {}
-        s_dict['id'] = s.id
-        s_dict['name'] = s.value
-
-        d_out['children'].append(s_dict)
+    d_out['children'] = [{"id": s.id, "name": s.value} for s in subraces]
 
     d_out["children"] = sorted(d_out["children"], key=lambda r: r["name"])
 
@@ -108,16 +76,7 @@ def get_classes():
     classes = current_app.db.session.query(CharacterClass)
 
     d_out = dict()
-    d_out["parents"] = []
-
-    for c in classes:
-        subclasses = get_subclasses(c.id)
-        r_dict = {}
-        r_dict['id'] = c.id
-        r_dict['name'] = c.value
-        r_dict['children'] = len(subclasses['children'])
-
-        d_out['parents'].append(r_dict)
+    d_out["parents"] = [{"id": c.id, "name": c.value, "children": len(get_subclasses(c.id)['children'])} for c in classes]
 
     d_out["parents"] = sorted(d_out["parents"], key=lambda r: r["name"])
 
@@ -128,14 +87,7 @@ def get_subclasses(c):
     subclasses = current_app.db.session.query(CharacterSubclass).filter(CharacterSubclass.parent == c)
 
     d_out = dict()
-    d_out['children'] = []
-
-    for s in subclasses:
-        s_dict = {}
-        s_dict['id'] = s.id
-        s_dict['name'] = s.value
-
-        d_out['children'].append(s_dict)
+    d_out['children'] = [{"id": s.id, "name": s.value} for s in subclasses]
 
     d_out["children"] = sorted(d_out["children"], key=lambda r: r["name"])
 
