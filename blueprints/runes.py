3e6b795fab6f95bf937460e755c4fd429b62e309
Bryce Hadsell <bryce.hadsell@gmail.com>
2023-09-10 20:16:29 -0500

    Runes testing

--- blueprints/runes.py
+++ blueprints/runes.py
@@ -5,30 +5,218 @@ from flask import Blueprint, render_template, url_for, current_app, session
 
 runes_blueprint = Blueprint("rune", __name__)
 
+die_size_map = {4: "1d6", 6: "1d8", 8: "2d4", 10: "1d12", 12: "2d6"}
+
+
 @runes_blueprint.route('/')
 def runes():
     f = open('json/rune.json', encoding="utf8")
-    info = json.load(f)
-    total_url = url_for('rune.total')
-    return render_template('/runes/runes.html', info=info, total_url=total_url)
+    runes = json.load(f)
 
-@runes_blueprint.route('/total', methods=['POST'])
-def total():
-    form = flask.request.json
+    runes = sorted(runes, key=lambda r: r["name"])
+
+    return render_template('/runes/runes.html', runes=runes)
+
+
+@runes_blueprint.route('/build_weapon', methods=['POST'])
+def weapon():
+    form = flask.request.form.to_dict()
+
+    weapon = buildWeapon(form)
+
+    return json.dumps(weapon)
+
+
+def buildWeapon(form):
+    out = []
+
+    attackMod = 'max(dexterityMod,strengthMod)' if form.get('weapon-type-finesse') else 'strengthMod' if form.get(
+        'weapon-type') == "melee" else 'dexterityMod'
+    damage = {"type": "damage", "damage": f"{form.get('damage-die')}+{{{attackMod}}} [{form.get('damage-type')}]"}
+    attack = {"type": "attack", "attackBonus": f"proficiencyBonus+{attackMod}", "hit": [damage], "miss": []}
+    target = {"type": "target", "target": "all", "effects": [attack]}
+    base = {"name": form.get('weapon-name'), "_v": 2, "automation": [target], "criton": form.get('crit-range')}
+
+    if form.get('weapon-text'):
+        text = {"type": "text", "text": form.get('weapon-text'), "title": "Effect"}
+        base['automation'].append(text)
+
+    if form.get('weapon-type-versatile'):
+        die = form.get('damage-die')
+        size = int(die[die.find("d")+1:])
+        try:
+            new = json.dumps(base).replace(die, die_size_map[size]).replace(form.get('weapon-name'), f'2-Handed {form.get("weapon-name")}')
+            out.append(json.loads(new))
+        except:
+            pass
+
+    out.append(base)
+
+    weapons = processRunes(out, form)
+
+    return weapons
+
+def processRunes(attacks, form):
     f = open('json/rune.json', encoding="utf8")
-    info = json.load(f)
-    sum = 0
-    cost = 0
+    format = ["#name#", "#damageMod#", "#dtype#", "#flavorText#", "#criton#", "#attackBonus#", "#upDie1#", "#upDie2#"]
+    runes = json.load(f)
+    flavorText = form.get('weapon-text')
+    out = []
 
-    for field in form:
-        if field in [n["name"] for n in info["runes"]]:
-            rune = next((x for x in info["runes"] if x["name"] == field),None)
-            value = int(form[field]) if form[field] else 0
-            adj = float(rune.get("point",0))
-            sum += value * adj
-            cost += int(rune.get("cost",0))*value
-    output = {
-        "total": f"{str(int(sum) if sum % 1 ==0 else sum)}",
-        "cost": f"{str(int(cost))}"
-    }
-    return output
+    for attack in attacks:
+        name = attack.get("name")
+        a = json.dumps(attack)
+        test = getUpDie(a, 1)
+        for rune in runes:
+            if form.get(rune.get('name')):
+                match rune.get('type'):
+                    case "flavor":
+                        pass
+                    case "hit_bonus":
+                        val = get_rune_value(rune, form)
+                        a = a.replace('attackBonus": "', f'attackBonus": "{val}+')
+
+                    case "dmg_bonus":
+                        val = get_rune_value(rune, form)
+                        a = a.replace('} [', '}' + f'+{val} [')
+
+                    case "stat":
+                        val = get_rune_value(rune, form)
+                        bonus = get_attack_bonus(a)
+                        if val in bonus:
+                            pass
+                        elif "dexterityMod" in bonus:
+                            a = a.replace('dexterityMod', val)
+                        elif "strengthMod" in bonus:
+                            a = a.replace('strengthMod', val)
+
+                    case "hit_effect":
+                        if save := rune.get("save"):
+                            dc = 12 + int(get_rune_value(rune, form))
+                            s = {"type": "save", "stat": save.get("type"), "fail": [], "success": [], "dc": dc}
+                            if fail := save.get("fail"):
+                                if damage := fail.get("damage"):
+                                    dmg = {"type": "damage", "damage": damage}
+                                    s["fail"].append(dmg)
+
+                                if effect := fail.get("effect"):
+                                    e = {"type": "ieffect2", "name": effect.get("name"),
+                                         "duration": effect.get("duration", -1),
+                                         "effects": effect.get("effects"),
+                                         "end": effect.get("end", False),
+                                         "conc": effect.get("conc", False),
+                                         "desc": effect.get("desc", ""),
+                                         "tick_on_caster": effect.get("tick_on_caster", False)}
+                                    s["fail"].append(e)
+
+                            if success := save.get("success"):
+                                if damage := success.get("damage"):
+                                    dmg = {"type": "damage", "damage": damage}
+                                    s["success"].append(dmg)
+
+                                if effect := success.get("effect"):
+                                    e = {"type": "ieffect2", "name": effect.get("name"),
+                                         "duration": effect.get("duration", -1),
+                                         "effects": effect.get("effects"),
+                                         "end": effect.get("end", False),
+                                         "conc": effect.get("conc", False),
+                                         "desc": effect.get("desc", ""),
+                                         "tick_on_caster": effect.get("tick_on_caster", False)}
+                                    s["success"].append(dmg)
+
+                        a = append_on_hit(a, s)
+
+                    case "attack":
+                        attackMod = get_attack_stat(a)
+                        attackBonus = get_attack_bonus(a)
+                        dtype = get_damage_type(a)
+                        criton = int(get_criton(a))
+                        on_hit = get_on_hit_effects(a)
+                        upDie1 = getUpDie(a)
+                        upDie2 = getUpDie(a, 1)
+
+                        r = [name, attackMod, dtype, flavorText, criton, attackBonus, upDie1, upDie2]
+                        if new_atk := rune.get("attack"):
+                            for i in range(len(format)):
+                                new_atk = new_atk.replace(format[i], str(r[i]))
+
+                            for eff in on_hit:
+                                new_atk = append_on_hit(new_atk, eff)
+
+                            if rune.get("replace"):
+                                a = new_atk
+                            else:
+                                out.append(json.loads(new_atk))
+
+        out.append(json.loads(a))
+
+    return out
+
+def get_rune_value(rune, form):
+    return rune.get('value').replace('#upgrade#', form.get(f'{rune.get("name")}-upgrade',""))
+
+def get_attack_stat(a):
+    bonus = get_attack_bonus(a)
+    return "max(dexterityMod,strengthMod)" if "max" in bonus else "dexterityMod" if "dex" in bonus else "strengthMod"
+def get_attack_bonus(a):
+    index = a.find('"attackBonus": "') + 16
+    return str(a[index:a[index:].find('"') + index])
+
+def get_damage_type(a):
+    attack_str = get_attack_str(a)
+
+    return str(attack_str[attack_str.find('[') + 1:attack_str.find(']')])
+
+def get_attack_str(a):
+    dindex = a.find('"damage": "') + 11
+
+    attack_str = str(a[dindex:a[dindex:].find('"') + dindex])
+    return attack_str
+
+def get_criton(a):
+    index = a.find('"criton": ') + 11
+    crit = str(a[index:a[index:].find('}')-1 + index])
+    return crit
+
+def get_damage_die(a):
+    dindex = a.find('"damage": "') + 11
+    damage_str = str(a[dindex:a[dindex:].find('+') + dindex])
+    return damage_str
+
+def append_on_hit(a, automation):
+    atk = json.loads(a)
+    for effect in atk["automation"]:
+        if 'effects' in effect:
+            for effect_type in effect['effects']:
+                if 'hit' in effect_type:
+                    effect_type['hit'].append(automation)
+                    break
+
+    return json.dumps(atk)
+
+def getUpDie(a, add_steps=0):
+    attackDie = get_damage_die(a)
+    size = int(attackDie[attackDie.find("d") + 1:])
+    tmp = list(die_size_map.items())
+    idx = [i for i, k in enumerate(tmp) if k[0] == size][0]+add_steps
+
+    if attackDie == "2d6" or idx > len(die_size_map)-1:
+        return "2d6"
+
+    return list(die_size_map.values())[idx]
+
+def get_on_hit_effects(a):
+    atk = json.loads(a)
+    eff = None
+
+    for effect in atk["automation"]:
+        if "target" in effect:
+            for effect_type in effect["effects"]:
+                if "hit" in effect_type:
+                    eff = [e for e in effect_type['hit'] if e["type"] != "damage"]
+                    break
+
+    return eff
+
+
+
--- static/js/rune.js
+++ static/js/rune.js
@@ -1,27 +1,131 @@
-
-
-function getTotal(){
-    document.addEventListener('click', function(event){
-        fetch(url)
-            .then(console.log(url))
-
-        var form = document.forms.runeForm;
-        var data = new FormData(form);
-        var object = {}
-        data.forEach((value, key) => object[key] = value);
-        $.ajax({
-            url: "/runes/total",
-            data: JSON.stringify(object),
-            contentType: 'application/json;charset=UTF-8',
-            type: 'POST',
-            success: function(response){
-                console.log(response)
-
-                var x = document.getElementById("totalPoints");
-                x.innerHTML = response.total
+function onLoad(){
+    // Button
+    document.getElementById('generate-button').addEventListener('click', function(event){
+        var fields = {}
+        $('#weaponForm').find("input, textarea").each(function(){
+            if (this.type == 'radio' || this.type == 'checkbox'){
+                if (this.checked){
+                    fields[this.name] = $(this).val()
+                }
+            } else{
+                fields[this.name] = $(this).val()
             }
-        });
+        })
+
+        console.log(fields)
+
+        if (fields['weapon-name'] !== "" && fields['damage-die'] !== "" && fields['damage-type'] !== ""){
+            $.ajax({
+                url: document.getElementById('weaponURL').innerHTML,
+                data: fields,
+                dataType: 'json',
+                type: "post",
+                success: function(response){
+                    document.getElementById('command-text').innerHTML = `!a import ` + JSON.stringify(response)
+                    $('#weaponModal').modal('show')
+
+                    document.getElementById('command-copy').addEventListener('click', function(event){
+                        var text = document.getElementById('command-text').innerHTML
+
+                        if (text){
+                            navigator.clipboard.writeText(text)
+                        }
+                    })
+                }
+            })
+        }
+    })
+
+    // Runes
+    var all_runes = document.getElementsByClassName('rune')
+    Array.from(all_runes).forEach((rune) => {
+        console.log(rune)
+        document.getElementById(rune.id).onclick = function(){
+            var runeDom = document.getElementById('rune-count')
+            var runeCount = parseInt(runeDom.innerHTML)
+            var runeMax = parseInt(document.getElementById('rune-total').innerHTML)
+
+            if (this.checked){
+                runeCount += 1
+            } else {
+                runeCount -= 1
+            }
+
+            runeDom.innerText = runeCount
+
+            if (runeCount == runeMax){
+                runeDom.classList.add("text-success")
+                runeDom.classList.remove("text-danger")
+            } else if (runeCount < runeMax){
+                runeDom.classList.remove("text-success")
+                runeDom.classList.remove("text-danger")
+            } else if (runeCount > runeMax){
+                runeDom.classList.add("text-danger")
+                runeDom.classList.remove("text-success")
+            }
+
+        }
+    })
+
+    // Upgrades
+    var upgradeable_runes = document.getElementsByClassName('upgrade')
+    Array.from(upgradeable_runes).forEach((rune) =>{
+        document.getElementById(rune.id).onchange = function(){
+            var sections = document.getElementsByClassName(`${rune.id}-upgrades`)
+            Array.from(sections).forEach((section) =>{
+                if (this.checked){
+                    section.hidden=false
+                } else {
+                    section.hidden=true
+                }
+            })
+        }
+    })
+
+    // Inputs
+    var inputFields = document.querySelectorAll('input')
+    inputFields.forEach(input => {
+        input.addEventListener('input', saveToLocalStorage)
+        loadFromLocalStorage(input)
+    })
+
+    var textArea = document.querySelectorAll('textarea')
+    textArea.forEach(input =>{
+        input.addEventListener('input', saveToLocalStorage)
+        loadFromLocalStorage(input)
     })
 }
 
-getTotal()
+function saveToLocalStorage(){
+    var weapon = JSON.parse(localStorage.getItem("customWeapon") || "{}")
+    var inputName = this.name
+    var inputValue = this.value
+    if (this.type == 'checkbox' && this.checked == false){
+        delete weapon[inputName]
+    } else {
+        weapon[inputName] = inputValue
+    }
+    localStorage.setItem("customWeapon", JSON.stringify(weapon))
+}
+
+function loadFromLocalStorage(input){
+    var weapon = JSON.parse(localStorage.getItem("customWeapon") || "{}")
+    var inputName = input.name
+    var value = weapon[inputName]
+    var change = new Event('change')
+    var click = new Event('click')
+
+    if (value !== null && value){
+        if (input.type === 'radio' || input.type == 'checkbox'){
+            if (input.value == value){
+                input.checked = true
+                input.dispatchEvent(change)
+                input.dispatchEvent(click)
+            }
+        } else{
+            input.value = value
+        }
+    }
+}
+
+onLoad()
